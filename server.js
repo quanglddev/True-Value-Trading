33fcc9611619464fe441e411005959c8b4319693
Quang Luong <crzqag@Quangs-MacBook-Pro.local>
2020-09-24 19:22:13 -0400

    Everything

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.set('port', port);
 // Set static path to Angular app in dist
 // Don't run in dev
 if (process.env.NODE_ENV !== 'dev') {
-    app.use('/', express.static(path.join(__dirname, './dist')));
+    app.use('/', express.static(path.join(__dirname, './dist/stock-bot-angular')));
 }
 
 /*
@@ -67,7 +67,7 @@ require('./server/api')(app, config);
 // Don't run in dev
 if (process.env.NODE_ENV !== 'dev') {
     app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, '/dist/index.html'));
+        res.sendFile(path.join(__dirname, '/dist/stock-bot-angular/index.html'));
     });
 }
 
